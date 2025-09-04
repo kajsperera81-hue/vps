171a129f4d67b83ca7a786feb84e12c8e72602bf
gesandu1111 <gesandusanmira@gmail.com>
2025-09-04 06:08:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,15 @@
-// bot.js
 const TelegramBot = require('node-telegram-bot-api');
 
-// BotFather token paste කරන්න
 const token = '8402815314:AAHjQwTP9NdUaToeuj2Y-JaPbrBi4ZSat00';
 
-// Polling mode
 const bot = new TelegramBot(token, { polling: true });
 
-// /start command reply
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}! Bot is running 🚀`);
 });
 
-// Simple text reply
 bot.on('message', (msg) => {
-  if (msg.text.toLowerCase() === 'hi') {
+  if (msg.text && msg.text.toLowerCase() === 'hi') {
     bot.sendMessage(msg.chat.id, 'Hello! How are you? 😊');
   }
 });
